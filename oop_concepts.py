4dc9061c9e847f3e1cdf35bbf1484c04aa5d01c1
Harsha C K <harshack.21kumar@gmail.com>
2026-07-24 21:51:13 +0530

    Introduce car class with brand and model attributes
    Added a new class 'car' with attributes 'brand' and 'model'.

--- oop_concepts.py
+++ oop_concepts.py
@@ -6,3 +6,11 @@ class student:
 s1=student(19,"harsha")
 print(s1.age)
 print(s1.name)
+#program 2:
+class car:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+c1=car("Mercedes-Benz","C-Class")
+print(c1.brand)
+print(c1.model)
